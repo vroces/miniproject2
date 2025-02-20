fc38eec45a94c203f61023f4c63cc8d958b23405
vroces <roces510@icloud.com>
2025-02-20 10:03:32 -0500

    update

--- mini-project2/src/pages/WhereWePlayPage.jsx
+++ mini-project2/src/pages/WhereWePlayPage.jsx
@@ -1,21 +1,28 @@
-// WhereWePlayPage.jsx
 import React from "react";
-
-import Header from "../components/Header"; // Import your Header component
-import Footer from "../components/Footer"; // Import your Footer component
+import Header from "../components/Header"; 
+import Footer from "../components/Footer"; 
 
 const WhereWePlayPage = () => {
   return (
-    <div className="weplaypage-container"> {/* Optional: Add a container for the entire page */}
+    <div className="weplaypage-container"> 
       
       <div className="weplayheader-container">
-        <Header /> {/* Add the Header component inside its own container */}
+        <Header />
       </div>
       
-      
+      <div className="weplay-content">
+       <iframe 
+          src="https://app.mapline.com/map/map_34d1986d/bTBXb2c2YklFdmxyQWU1bi9HZnBWRHA3VkFBUmtiZEtEVVVjTm" 
+          style={{ width: "100%", height: "560px", border: "none" }} 
+          allow="geolocation *"
+        ></iframe>
+        <div style={{ fontSize: "10px", textAlign: "center", marginTop: "10px" }}>
+          <a href="https://mapline.com" target="_blank" rel="noopener noreferrer">Mapping by Mapline</a>
+        </div>
+      </div>
       
       <div className="footer-container">
-        <Footer /> {/* Add the Footer component inside its own container */}
+        <Footer />
       </div>
       
     </div>
